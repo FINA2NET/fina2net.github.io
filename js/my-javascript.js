9c7b1557bf0f39b8492585493ae09a790f5d2ef7
aakkaakkii <aakkaakkii1997@gmail.com>
2018-03-12 10:57:44 +0400

    v3

---
+++ js/my-javascript.js
@@ -0,0 +1,24 @@
+function myFunction(){
+    var x = document.getElementById("social-links-container");
+
+    if(x.className==="social-links-class")
+        x.className="social-links-class-response";
+    else
+        x.className="social-links-class";
+}
+
+var createAccordian = function(accordianElem) {
+    if(accordianElem.is( ":hidden" ))
+        accordianElem.slideDown();
+    else
+        accordianElem.hide("slow");
+};
+
+$(document).ready(function(){
+    $(".accordian-element").hide();
+    $(".accordian-title").click(function(){
+        createAccordian($(this).next());
+    });
+});
+
+$('.dropdown-toggle').dropdownHover(options);
