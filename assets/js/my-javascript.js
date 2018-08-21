3c3208bf40e8a052ddbe40abb7f2cfe557a7443c
aakkaakkii <aakkaakkii1997@gmail.com>
2018-08-21 23:03:16 +0400

    new version

--- assets/js/my-javascript.js
+++ assets/js/my-javascript.js
@@ -38,3 +38,42 @@ $(document).ready(function(){
         createAccordian($(this).next());
     });
 });
+
+
+
+function responsiveClass(target, cssClass, flag) {
+    $(document).ready(function () {
+        if (window.innerWidth < 780 === flag)
+            $(target).addClass(cssClass);
+        else
+            $(target).removeClass(cssClass);
+    });
+
+    window.onresize = () => {
+        if (window.innerWidth < 780 === flag)
+            $(target).addClass(cssClass);
+        else
+            $(target).removeClass(cssClass);
+    }
+}
+
+
+
+//share
+SocialShareKit.init({
+    twitter: {
+        title: 'FINA LLC',
+        via: 'fina2.net'
+    },
+    onBeforeOpen: function (targetElement, network, paramsObj) {
+        console.log(arguments);
+    },
+    onOpen: function (targetElement, network, url, popupWindow) {
+        console.log(arguments);
+    },
+    onClose: function (targetElement, network, url, popupWindow) {
+        console.log(arguments);
+    }
+});
+
+
--- assets/js/vacancies.js
+++ assets/js/vacancies.js
@@ -5,16 +5,34 @@ $(document).ready(function () {
         for(let i=0; i<json.vacancies.length;i++)
             $('.vacancies-head').append(
                 '<div id="'+json.vacancies[i].id+'" class="row padding-top padding-bottom vacancies-card">'
-                +'<div class="col-lg-4 col-md-4">'
+                +'<div class="col-lg-2 col-sm-3 col-12">'
                 +'<img class="vacancies-img" src="'+json.vacancies[i].img +'">'
                 +'</div>'
-                +'<div class="col-lg-8 col-md-8">'
+                +'<div class="col-lg-10 col-sm-9 col-12">'
                 +'<div class="vacancies-title">'+json.vacancies[i].title +'</div>'
                 +'<hr class="vacancies-hr">'
-                +'<div class="vacancies-text">'+json.vacancies[i].description+'</div>'
-                +'<div class="text-center"><div class="btn my-btn"><a href="'+json.vacancies[i].formLink+'" target="_blank" style="color:white!important;"><strong>Application Form</strong></a></div></div>'
+                +'<div class="vacancies-text">'+json.vacancies[i].shortDescription+'</div>'
+                +'<div class="text-center"><div class="btn my-btn vacan-btn"  data-vacanID="'+json.vacancies[i].id+'"><strong>Read more</strong></div></div>'
                 +'</div>'
                 +'</div>'
             );
     });
 });
+
+
+$(document).on('click',".vacan-btn",function () {
+    console.log($(this).attr('data-some'));
+   window.location = updateQueryStringParameter(window.location.href.replace('vacancies','vacancion'), 'vacan',$(this).attr('data-vacanID'));
+});
+
+
+function updateQueryStringParameter(uri, key, value) {
+    const re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+    const separator = uri.indexOf('?') !== -1 ? "&" : "?";
+    if (uri.match(re)) {
+        return uri.replace(re, '$1' + key + "=" + value + '$2');
+    }
+    else {
+        return uri + separator + key + "=" + value;
+    }
+}
