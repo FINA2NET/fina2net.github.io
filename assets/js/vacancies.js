2762c7e768ba9c8173782fcd556a8b57303fdeca
aakkaakkii <aakkaakkii1997@gmail.com>
2019-03-11 17:57:22 +0400

    fix vacancies buttons

--- assets/js/vacancies.js
+++ assets/js/vacancies.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
                 +'</div>'
                 +'<div class="col-sm-9 offset-sm-3 col-12">'
                 +'<div class="text-center">'
-                +'<a href="' + json.vacancies[i].formLink + '" target="_blank" style="color:white!important;"><div class="btn my-btn mr-2 pl-5 pr-5"><strong>Application Form</strong></div></a>'
+                +'<a href="' + json.vacancies[i].formLink + '" target="_blank" style="color:white!important;"><div class="btn my-btn mr-2"><strong>Application Form</strong></div></a>'
                 +'<div class="btn my-btn vacan-btn"  data-vacanID="'+json.vacancies[i].id+'"><strong>Read more</strong></div>'
                 +'</div>'
                 +'</div>'
