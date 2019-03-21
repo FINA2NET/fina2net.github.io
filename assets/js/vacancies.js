62c7e167ddf53581a5d0341fc61eac877332b6a7
aakkaakkii <aakkaakkii1997@gmail.com>
2019-03-21 12:11:59 +0400

    add interns vacancy

--- assets/js/vacancies.js
+++ assets/js/vacancies.js
@@ -3,31 +3,58 @@
 $(document).ready(function () {
     $.getJSON("vacancies.json", function(json) {
         for(let i=0; i<json.vacancies.length;i++)
-            $('.vacancies-head').append(
-                '<div id="'+json.vacancies[i].id+'" class="row padding-top padding-bottom vacancies-card">'
-                +'<div class="col-lg-3 col-sm-3 col-12">'
-                +'<img class="vacancies-img" src="'+json.vacancies[i].img +'">'
-                +'</div>'
-                +'<div class="col-lg-9 col-sm-9 col-12">'
-                +'<div class="vacancies-title">'+json.vacancies[i].title +'</div>'
-                +'<hr class="vacancies-hr">'
-                +'<div class="vacancies-text">'+json.vacancies[i].shortDescription+'</div>'
-                +'</div>'
-                +'<div class="col-sm-9 offset-sm-3 col-12">'
-                +'<div class="text-center">'
-                +'<a href="' + json.vacancies[i].formLink + '" target="_blank" style="color:white!important;"><div class="btn my-btn mr-2"><strong>Application Form</strong></div></a>'
-                +'<div class="btn my-btn vacan-btn"  data-vacanID="'+json.vacancies[i].id+'"><strong>Read more</strong></div>'
-                +'</div>'
-                +'</div>'
-                +'</div>'
-            );
+
+            if(json.vacancies[i].id=="interns"){
+                $('.vacancies-head').append(
+                    '<div id="' + json.vacancies[i].id + '" class="row padding-top padding-bottom vacancies-card">'
+                    + '<div class="col-lg-3 col-sm-3 col-12">'
+                    + '<img class="vacancies-img" src="' + json.vacancies[i].img + '">'
+                    + '</div>'
+                    + '<div class="col-lg-9 col-sm-9 col-12">'
+                    + '<div class="vacancies-title">' + json.vacancies[i].title + '</div>'
+                    + '<hr class="vacancies-hr">'
+                    + '<div class="vacancies-text">' + json.vacancies[i].shortDescription + '</div>'
+                    + '</div>'
+                    + '<div class="col-sm-9 offset-sm-3 col-12">'
+                    + '<div class="text-center">'
+                    + '<a href="' + json.vacancies[i].formLink + '" target="_blank" style="color:white!important;"><div class="btn my-btn mr-2"><strong>Application Form</strong></div></a>'
+                    + '<div class="btn my-btn vacan-btn"  data-vacanID="' + json.vacancies[i].id + '"><strong>Read more</strong></div>'
+                    + '</div>'
+                    + '</div>'
+                    + '</div>'
+                );
+            }else {
+
+                $('.vacancies-head').append(
+                    '<div id="' + json.vacancies[i].id + '" class="row padding-top padding-bottom vacancies-card">'
+                    + '<div class="col-lg-3 col-sm-3 col-12">'
+                    + '<img class="vacancies-img" src="' + json.vacancies[i].img + '">'
+                    + '</div>'
+                    + '<div class="col-lg-9 col-sm-9 col-12">'
+                    + '<div class="vacancies-title">' + json.vacancies[i].title + '</div>'
+                    + '<hr class="vacancies-hr">'
+                    + '<div class="vacancies-text">' + json.vacancies[i].shortDescription + '</div>'
+                    + '</div>'
+                    + '<div class="col-sm-9 offset-sm-3 col-12">'
+                    + '<div class="text-center">'
+                    + '<a href="' + json.vacancies[i].formLink + '" target="_blank" style="color:white!important;"><div class="btn my-btn mr-2"><strong>Application Form</strong></div></a>'
+                    + '<div class="btn my-btn vacan-btn"  data-vacanID="' + json.vacancies[i].id + '"><strong>Read more</strong></div>'
+                    + '</div>'
+                    + '</div>'
+                    + '</div>'
+                );
+            }
     });
 });
 
 
 $(document).on('click',".vacan-btn",function () {
-    console.log($(this).attr('data-some'));
-   window.location = updateQueryStringParameter(window.location.href.replace('vacancies','vacancion'), 'vacan',$(this).attr('data-vacanID'));
+    console.log($(this).attr('data-vacanID'));
+    if($(this).attr('data-vacanID')=="interns"){
+        window.location = window.location.href.replace('vacancies','interns');
+    }else{
+        window.location = updateQueryStringParameter(window.location.href.replace('vacancies','vacancion'), 'vacan',$(this).attr('data-vacanID'));
+    }
 });
 
 
