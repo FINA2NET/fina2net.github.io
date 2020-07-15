6298d32656e59f1c62e65c6c54a3dfa7adb7ce2f
aakkaakkii <aakkaakkii1997@gmail.com>
2020-07-15 17:57:37 +0400

    FINA-2026

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest, watch, parallel } = require('gulp');
+const {src, dest, watch, parallel} = require('gulp');
 let sass = require('gulp-sass');
 let cleanCSS = require('gulp-clean-css');
 let rename = require("gulp-rename");
@@ -17,6 +17,12 @@ function html() {
         .pipe(dest('./'))
 }
 
+function vacancyHtml() {
+    return src('./src/pug/vacancies/*.pug')
+        .pipe(pug())
+        .pipe(dest('./'))
+}
+
 function css() {
     return src('./src/scss/**/*.scss')
         .pipe(sass())
@@ -26,20 +32,19 @@ function css() {
         }))
         .pipe(cleanCSS())
         .pipe(dest('./assets/css'))
-
 }
 
 function js() {
-    return src('./assets/js/*.js', { sourcemaps: true })
+    return src('./assets/js/*.js', {sourcemaps: true})
         .pipe(concat('app.min.js'))
-        .pipe(dest('./assets/js', { sourcemaps: true }))
+        .pipe(dest('./assets/js', {sourcemaps: true}))
 }
 
 function material(){
     return src('./assets/scss/material-kit.scss').pipe(sass()).pipe(dest('./'));
 }
 
-function robot(){
+function robot() {
     return src('./*.html')
         .pipe(robots({
             useragent: '*',
@@ -50,12 +55,12 @@ function robot(){
         .pipe(dest('./'));
 }
 
-function sitemapInit(){
+function sitemapInit() {
     return src('*.html', {
         read: false
     }).pipe(save('before-sitemap'))
         .pipe(sitemap({
-            siteUrl: 'http://fina.bi/',
+            siteUrl: 'https://fina2.net',
             mappings: [{
                 pages: ['clients.html', 'partners.html'],
                 changefreq: 'hourly',
@@ -72,12 +77,12 @@ function sitemapInit(){
 
 exports.js = js;
 exports.css = css;
-exports.html = html;
+exports.html = parallel(html, vacancyHtml);
 exports.material = material;
 exports.robot = robot;
 exports.sitemapInit = sitemapInit;
-exports.buildAll = parallel(html, css, js);
-exports.default = function() {
+exports.buildAll = parallel(html, js);
+exports.default = function () {
     watch('./src/pug/**/*', html);
     watch('./src/scss/**/*.scss', css);
 };
